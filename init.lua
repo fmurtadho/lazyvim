f273721b0db350be60ca5c7df05d735c44d00467
Muhammad Faishal Murtadho <fmurtadho@gmail.com>
2024-09-05 18:58:06 +0700

    change node version

--- init.lua
+++ init.lua
@@ -2,6 +2,6 @@
 require("config.lazy")
 
 -- Path to Node 20 bin directory
-local node20_bin_path = "/Users/24061754/.nvm/versions/node/v20.15.1/bin"
+local node20_bin_path = "/Users/24061754/.nvm/versions/node/v20.17.0/bin"
 -- Prepend Node 20's bin path to the existing PATH environment variable
 vim.env.PATH = node20_bin_path .. ":" .. vim.env.PATH
