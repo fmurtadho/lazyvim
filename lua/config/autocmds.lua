71b8360bf2caf7ab9842ff1e94bf79585d464a89
MUHAMMAD FAISHAL MURTADHO <fmurtadho@gmail.com>
2025-10-02 13:21:57 +0700

    disable autoformat, add LspEslintAutoFix to autocmd, update packages

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,13 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local eslint_group = vim.api.nvim_create_augroup("EslintFixOnSave", { clear = true })
 
-local env_group = vim.api.nvim_create_augroup("EnvFiles", { clear = true })
-
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  group = env_group,
-  pattern = { ".env", ".env.*" },
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = eslint_group,
+  pattern = { "*.js", "*.ts", "*.jsx", "*.tsx" },
   callback = function()
-    vim.b.autoformat = false
+    vim.cmd("LspEslintFixAll")
   end,
 })
