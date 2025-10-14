fcbd79a35a0c7d2f009f5b1e26fcbc0fdbc70949
Muhammad Faishal Murtadho <fmurtadho@gmail.com>
2025-10-14 10:16:02 +0700

    add leader cr to remove \\n \\t \\r from buffer, and cf to format with dprint

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,11 @@ local map = vim.keymap.set
 -- Escape insert mode with jk or jj
 map("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
 map("i", "jj", "<Esc>", { desc = "Exit insert mode with jj" })
+
+map("n", "<leader>cr", [[:%s/\\[ntr]//g<CR>]], { desc = "Remove \\n, \\t, \\r from buffer" })
+
+map("n", "<leader>cf", function()
+  vim.cmd("write") -- save first
+  vim.cmd("!dprint fmt %")
+  vim.cmd("edit!") -- reload file after formatting
+end, { desc = "Format current buffer with dprint" })
